40cc60264c7a91c0b93f7597637f9ab931d5e85e
junnys6018 <junkanglim1@gmail.com>
2019-12-28 22:30:19 +0100

    moved tests into its own directory

--- openGL/src/tests/__Test__.h
+++ openGL/src/tests/__Test__.h
@@ -1,27 +1,27 @@
 #ifndef __TEST__
 #define __TEST__
 
-	#include "testPlanets.h"
-	#include "testLighting.h"
-	#include "testTriangle.h"
-	#include "testDepthTest.h"
-	#include "testFrameBuf.h"
-	#include "testCubeMap.h"
-	#include "testAdvGLSL.h"
-	#include "testInstancing.h"
-	#include "testAdvLight.h"
-	#include "testShadows.h"
-	#include "testPointShadow.h"
-	#include "testNormalMap.h"
-	#include "testParallaxMap.h"
-	#include "testBloom.h"
-	#include "testDeferred.h"
-	#include "testSSAO.h"
+	#include "instances/testPlanets.h"
+	#include "instances/testLighting.h"
+	#include "instances/testTriangle.h"
+	#include "instances/testDepthTest.h"
+	#include "instances/testFrameBuf.h"
+	#include "instances/testCubeMap.h"
+	#include "instances/testAdvGLSL.h"
+	#include "instances/testInstancing.h"
+	#include "instances/testAdvLight.h"
+	#include "instances/testShadows.h"
+	#include "instances/testPointShadow.h"
+	#include "instances/testNormalMap.h"
+	#include "instances/testParallaxMap.h"
+	#include "instances/testBloom.h"
+	#include "instances/testDeferred.h"
+	#include "instances/testSSAO.h"
 
-	#include "PBR/testDirectPBR.h"
-	#include "PBR/testIBL_PBR.h"
+	#include "instances/PBR/testDirectPBR.h"
+	#include "instances/PBR/testIBL_PBR.h"
 
-	#include "test.h"
+	#include "instances/test.h"
 
 #endif
 
